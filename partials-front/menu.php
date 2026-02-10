76b6fa173aed19ce4473283afd8508422053d726
shizanmseleko3-tech <shizanmseleko3@gmail.com>
2026-02-10 22:49:01 +0300

    footer

---
+++ partials-front/menu.php
@@ -0,0 +1,54 @@
+<?php include('config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foodhub</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="<?php echo SITEURL; ?>" title="Logo">
+                    <img src="images/logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+            <br>
+            <div class="menu text-right">
+                <ul>
+                    <ul>
+                   <li><a href="<?php echo SITEURL; ?>">Home</a></li>
+                   <li><a href="<?php echo SITEURL; ?>categories.php">Categories</a></li>
+                   <li><a href="<?php echo SITEURL; ?>foods.php">Foods</a></li>
+
+
+                    <?php
+                    // Check if user is logged in
+                    if(empty($_SESSION["u_id"]))
+                    {
+                        // Not logged in
+                        echo '<li><a href="login.php">Login</a></li>';
+                    }
+                    else
+                    {
+                        // Logged in - show more links
+                        echo '<li><a href="myorders.php">My Orders</a></li>';
+                        echo '<li><a href="edit-profile.php">Edit Profile</a></li>';
+                        echo '<li><a href="logout.php">Logout</a></li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
